8dd3cd050eb2d063edda2e9f30c1443f8f7c11cf
Kajitsy <kajitsy@icloud.com>
2025-10-27 18:33:09 +0300

    Revert "Refactor file download threads and unify logic"
    This reverts commit 04cc2fb6606d1c24329ccf087d1bd9a48813242a.

--- src/modules/QThreads.py
+++ src/modules/QThreads.py
@@ -85,42 +85,48 @@ class ImageLoaderThread(QThread):
 class FileLoaderThread(QThread):
     file = pyqtSignal(object)
     error = pyqtSignal(str)
-    progress = pyqtSignal(int)
-    finished = pyqtSignal(str)
 
-    def __init__(self, url, headers={}, save_path=""):
+    def __init__(self, url, headers={}):
         super().__init__()
         self.url = url
         self.headers = headers
+
+    def run(self):
+        try:
+            response = requests.get(self.url, headers=self.headers)
+            if response.status_code == 200:
+                self.file.emit(response.content)
+            else:
+                logging.debug(f"QThreads.py ({self.__class__.__name__}.{inspect.currentframe().f_code.co_name}): File download error: {response.status_code}")
+        except Exception as e:
+            logging.debug(f"QThreads.py ({self.__class__.__name__}.{inspect.currentframe().f_code.co_name}): File download error: {e}")
+
+class DownloadThread(QThread):
+    progress = pyqtSignal(int)
+    finished = pyqtSignal(str)
+
+    def __init__(self, url, save_path):
+        super().__init__()
+        self.url = url
         self.save_path = save_path
 
     def run(self):
-        if self.save_path:
-            try:
-                response = requests.get(self.url, headers=self.headers, stream=True)
-                total_size = int(response.headers.get('content-length', 0))
-                downloaded_size = 0
+        try:
+            response = requests.get(self.url, stream=True)
+            total_size = int(response.headers.get('content-length', 0))
+            downloaded_size = 0
 
-                with open(self.save_path, 'wb') as file:
-                    for chunk in response.iter_content(4096):
-                        if chunk:
-                            file.write(chunk)
-                            downloaded_size += len(chunk)
-                            percent = int((downloaded_size / total_size) * 100)
-                            self.progress.emit(percent)
+            with open(self.save_path, 'wb') as file:
+                for chunk in response.iter_content(4096):
+                    if chunk:
+                        file.write(chunk)
+                        downloaded_size += len(chunk)
+                        percent = int((downloaded_size / total_size) * 100)
+                        self.progress.emit(percent)
 
-                self.finished.emit(self.save_path)
-            except Exception as e:
-                self.finished.emit(str(e))
-        else:
-            try:
-                response = requests.get(self.url, headers=self.headers)
-                if response.status_code == 200:
-                    self.file.emit(response.content)
-                else:
-                    logging.debug(f"QThreads.py ({self.__class__.__name__}.{inspect.currentframe().f_code.co_name}): File download error: {response.status_code}")
-            except Exception as e:
-                logging.debug(f"QThreads.py ({self.__class__.__name__}.{inspect.currentframe().f_code.co_name}): File download error: {e}")
+            self.finished.emit(self.save_path)
+        except Exception as e:
+            self.finished.emit(str(e))
 
 class PlayerThread(QThread):
     play_signal = pyqtSignal(object)
@@ -463,7 +469,7 @@ class ChatThread(QThread):
             response = await self.session.request(method, url, headers=headers, json=data, timeout=100)
         else:
             response = await self.session.request(method, url, headers=headers, data=data, timeout=100)
-        logging.debug(f"QThreads.py ({self.__class__.__name__}.{inspect.currentframe().f_code.co_name}): Async custom_request: {url}")
+        logging.debug(f"CustomCharAI.py ({self.__class__.__name__}.{inspect.currentframe().f_code.co_name}): Async custom_request: {url}")
         if response.status_code == 200:
             return response.json() if not text else json.loads(response.text)
         else:
