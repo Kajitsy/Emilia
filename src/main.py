fe1acb10e21f425edaa0fc330b92941244eaef17
Kajitsy <kajitsy@icloud.com>
2025-11-08 21:30:06 +0300

    Change Version

--- src/main.py
+++ src/main.py
@@ -112,7 +112,7 @@ class EmiliaNext(QMainWindow):
         self.drpc_show_username = self.settings.value("discord_rpc/show_username", False, type=bool)
         self.drpc_show_current_page = self.settings.value("discord_rpc/show_current_page", True, type=bool)
         self.svg_icons = Svg()
-        self.version = "3.2.0b"
+        self.version = "3.2.0"
         self.beta = version.parse(self.version).is_prerelease
 
         self.setGeometry(self.settings.value("main_window/x", 100, type=int), self.settings.value("main_window/y", 100, type=int),
