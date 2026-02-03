13f7f2685603c70adfdfe18cf8c238a4f6a86593
Kajitsy <kajitsy@icloud.com>
2026-02-03 11:49:02 +0300

    audio output change support has been added to the new voice chat.

--- src/modules/QThreads.py
+++ src/modules/QThreads.py
@@ -1,6 +1,7 @@
 import os, hashlib, logging, sounddevice, soundfile, io, asyncio, time, scipy.signal, inspect, json, re, uuid
 
 import curl_cffi.curl
+import numpy as np
 import requests, speech_recognition
 from PyQt6.QtCore import QThread, pyqtSignal, Qt, QRectF, QLocale
 from PyQt6.QtGui import QPixmap, QPainter, QPainterPath
@@ -708,7 +709,6 @@ class ChatThread(QThread):
                             break
                         else:
                             raise Exception(response.get('comment', 'Unknown error'))
-                        #response = await self.connect.new_chat(char, self.me['user']['id'], preferred_model_type=preferred_model_type, scene_id=scene_id)
                         logging.debug(f"QThreads.py ({self.__class__.__name__}.{inspect.currentframe().f_code.co_name}): New chat started")
                     break
                 except curl_cffi.curl.CurlError:
@@ -1236,20 +1236,20 @@ class VoiceModeThreadV2(QThread):
         self.chat_thread = self.mw.chat_thread
         self.eec = EEC(self.mw)
         self.vtube_studio = self.mw.settings.value("vtube/use", False, type=bool)
-        if self.mw.settings.value('input_device', False) is False:
-            self.input_index = 0
-        else:
-            self.input_index = self.mw.settings.value('input_device', 0, type=int) + 1
 
-        if self.mw.settings.value('output_device', False) is False:
-            self.output_index = 0
-        else:
-            self.output_index = self.mw.settings.value('output_device', 0, type=int) + 1
+        input_dev = self.mw.settings.value('input_device', False)
+        self.input_index = 0 if input_dev is False else self.mw.settings.value('input_device', 0, type=int) + 1
+
+        output_dev = self.mw.settings.value('output_device', False)
+        self.output_index = 0 if output_dev is False else self.mw.settings.value('output_device', 0, type=int)
 
         self.room = None
         self.audio_devices = None
         self.mic_track = None
         self.speaker_player = None
+
+        self.output_stream = None
+        self.is_bot_speaking = False
 
     def run(self):
         self.loop = asyncio.new_event_loop()
@@ -1259,14 +1259,15 @@ class VoiceModeThreadV2(QThread):
         try:
             self.loop.run_until_complete(self.start_call())
         except Exception as e:
-            print(str(e))
+            print(f"Critical Error in run: {e}")
+            self.speech_error_signal.emit(str(e))
         finally:
             pending = asyncio.all_tasks(self.loop)
             for task in pending:
                 task.cancel()
-
             if pending:
                 self.loop.run_until_complete(asyncio.gather(*pending, return_exceptions=True))
+            self.loop.close()
 
     async def start_call(self):
         self.session = curl_cffi.AsyncSession()
@@ -1280,23 +1281,36 @@ class VoiceModeThreadV2(QThread):
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0"
         }
 
-        payload = {
-            "enableASR": True,
-            "platform": "web",
-            "roomId": self.chat_id,
-            "rtcBackend": "lk",
-            "userAuthToken": self.token,
-            "username": self.username,
-            "voiceQueries": {},
-            "voices": {
-                self.char: self.voice_id
+        if self.voice_id:
+            payload = {
+                "enableASR": True,
+                "platform": "web",
+                "roomId": self.chat_id,
+                "rtcBackend": "lk",
+                "userAuthToken": self.token,
+                "username": self.username,
+                "voiceQueries": {},
+                "voices": {
+                    self.char: self.voice_id
+                }
             }
-        }
+        else:
+            payload = {
+                "enableASR": True,
+                "platform": "web",
+                "roomId": self.chat_id,
+                "rtcBackend": "lk",
+                "userAuthToken": self.token,
+                "username": self.username,
+                "voiceQueries": {
+                    self.char: self.char_name
+                },
+                "voices": {}
+            }
 
         async with curl_cffi.AsyncSession() as session:
             request = await session.post(url, headers=headers, json=payload, timeout=100)
             data = request.json()
-            print(data)
             call_token = data['lkToken']
             ws_url = data['lkUrl']
 
@@ -1306,22 +1320,16 @@ class VoiceModeThreadV2(QThread):
         @self.room.on("track_subscribed")
         def on_track_subscribed(track, publication, participant):
             if track.kind == rtc.TrackKind.KIND_AUDIO:
-                print("Персонаж говорит")
-                asyncio.create_task(self.speaker_player.add_track(track))
-
-        @self.room.on("track_unsubscribed")
-        def on_track_unsubscribed(track, publication, participant):
-            if track.kind == rtc.TrackKind.KIND_AUDIO:
-                print("Персонаж замолк")
-                asyncio.create_task(self.speaker_player.remove_track(track))
-
-        @self.room.on("disconnected")
-        def on_disconnected():
-            print("Звонок завершен.")
+                asyncio.create_task(self.handle_incoming_audio(track))
 
         try:
-            self.speaker_player = self.audio_devices.open_output()
-            await self.speaker_player.start()
+            self.output_stream = sounddevice.OutputStream(
+                channels=1,
+                samplerate=48000,
+                dtype='int16',
+                device=self.output_index
+            )
+            self.output_stream.start()
 
             await self.room.connect(url, token)
             self.connected_signal.emit(True)
@@ -1334,20 +1342,52 @@ class VoiceModeThreadV2(QThread):
             await self.room.disconnect()
             raise Exception(f"LiveKit error: {e}")
         finally:
-            if self.speaker_player:
-                await self.speaker_player.aclose()
+            if self.output_stream:
+                self.output_stream.stop()
+                self.output_stream.close()
             await self.room.disconnect()
             self.connected_signal.emit(False)
 
-    async def _enable_microphone(self):
-        try:
-            mic_device = self.audio_devices.open_input(input_device=self.input_index, enable_aec=True, noise_suppression=True)
-            self.mic_track = rtc.LocalAudioTrack.create_audio_track("microphone", mic_device.source)
+    async def handle_incoming_audio(self, track):
+        audio_stream = rtc.AudioStream(track)
 
-            await self.room.local_participant.publish_track(self.mic_track)
-            self.speech_signal.emit(True)
-        except Exception as e:
-            print(f"Не удалось включить микрофон: {e}")
+        silence_timer = 0
+        silence_threshold = 1
+
+        async for event in audio_stream:
+            if self.stop_event.is_set():
+                break
+
+            frame = event.frame
+            if not frame: continue
+            data_np = np.frombuffer(frame.data, dtype=np.int16)
+
+            self.output_stream.write(data_np)
+
+            if len(data_np) > 0:
+                rms = np.max(np.abs(data_np))
+            else:
+                rms = 0
+
+            if rms > 500:
+                if not self.is_bot_speaking:
+                    self.is_bot_speaking = True
+                    self.speech_signal.emit(False)
+                silence_timer = 0
+            else:
+                if self.is_bot_speaking:
+                    silence_timer += 0.01
+
+                    if silence_timer > silence_threshold:
+                        self.is_bot_speaking = False
+                        self.speech_signal.emit(True)
+
+    async def _enable_microphone(self):
+        mic_device = self.audio_devices.open_input(input_device=self.input_index, enable_aec=True, noise_suppression=True)
+        self.mic_track = rtc.LocalAudioTrack.create_audio_track("microphone", mic_device.source)
+
+        await self.room.local_participant.publish_track(self.mic_track)
+        self.speech_signal.emit(True)
 
     def stop_call(self):
         if hasattr(self, 'loop') and self.loop.is_running():
