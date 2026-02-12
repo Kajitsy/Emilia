4d2b939c422ca273c502fb64ea9c9c458316f220
Kajitsy <kajitsy@icloud.com>
2026-02-12 22:43:21 +0300

    New Voice Player

--- src/modules/cards/VoiceCards.py
+++ src/modules/cards/VoiceCards.py
@@ -10,6 +10,83 @@ from modules.style.Icons import Svg
 from modules.QThreads import PlayerThread, FileLoaderThread, VoiceModeThread, VoiceModeThreadV2
 from modules.style.Utils import color_avatar, format_text
 
+class _VoicePreviewController:
+    def __init__(self, main_window):
+        self.mw = main_window
+        self.svg_icons = Svg()
+        self.active_loader = None
+        self.active_player = None
+        self.active_button = None
+        self.is_playing = False
+        self.is_loading = False
+
+    def _set_button_play(self, button):
+        if button:
+            button.setIcon(self.svg_icons.play())
+
+    def _set_button_pause(self, button):
+        if button:
+            button.setIcon(self.svg_icons.pause())
+
+    def _reset_state(self):
+        self.active_loader = None
+        self.active_player = None
+        self.active_button = None
+        self.is_playing = False
+        self.is_loading = False
+
+    def stop(self):
+        if self.active_player:
+            self.active_player.stop()
+        if self.active_button:
+            self._set_button_play(self.active_button)
+        self._reset_state()
+
+    def toggle(self, url, button):
+        if not url:
+            return
+        if self.active_button is button and (self.is_playing or self.is_loading):
+            self.stop()
+            return
+
+        self.stop()
+        self.active_button = button
+        self.is_loading = True
+
+        loader = FileLoaderThread(url)
+        self.active_loader = loader
+        self.mw.threads.append(loader)
+        loader.file.connect(lambda data, loader_ref=loader: self._start_player(loader_ref, data))
+        loader.start()
+
+    def _start_player(self, loader, data):
+        if loader is not self.active_loader:
+            return
+        self.is_loading = False
+        player = PlayerThread(data)
+        self.active_player = player
+        self.mw.threads.append(player)
+        player.play_signal.connect(lambda _: self._on_play(player))
+        player.stop_signal.connect(lambda _: self._on_stop(player))
+        player.start()
+
+    def _on_play(self, player):
+        if player is not self.active_player:
+            return
+        self.is_playing = True
+        self._set_button_pause(self.active_button)
+
+    def _on_stop(self, player):
+        if player is not self.active_player:
+            return
+        self._set_button_play(self.active_button)
+        self._reset_state()
+
+def _preview_controller(main_window):
+    if not hasattr(main_window, "_voice_preview_controller"):
+        main_window._voice_preview_controller = _VoicePreviewController(main_window)
+    return main_window._voice_preview_controller
+
 class VoiceCard(QFrame):
     def __init__(self, main_window, data, character_id="", current_voice_id="", search=True):
         super().__init__()
@@ -36,38 +113,11 @@ class VoiceCard(QFrame):
         voice_frame.setLayout(voice_layout)
 
         play_button = QPushButton()
-        play_button.setIcon(Svg().play())
+        play_button.setIcon(self.svg_icons.play())
         play_button.setFixedWidth(40)
         play_button.setStyleSheet("background-color: transparent; color: #e8eaed; border: none; font-size: 32px;")
-        play_button.clicked.connect(lambda event: play(self.data.get('previewAudioURI')))
+        play_button.clicked.connect(lambda _=False: _preview_controller(self.mw).toggle(self.data.get('previewAudioURI'), play_button))
         voice_layout.addWidget(play_button)
-
-        def set_play():
-            try:
-                play_button.setIcon(self.svg_icons.play())
-                play_button.clicked.connect(lambda event: play(self.data.get('previewAudioURI')))
-            except:
-                pass
-
-        def set_pause(thread):
-            try:
-                play_button.setIcon(self.svg_icons.pause())
-                play_button.clicked.connect(lambda event: thread.stop())
-            except:
-                pass
-
-        def _play(dataa):
-            thread = PlayerThread(dataa)
-            thread.play_signal.connect(lambda: set_pause(thread))
-            thread.stop_signal.connect(lambda: set_play())
-            self.mw.threads.append(thread)
-            thread.start()
-
-        def play(url):
-            thread = FileLoaderThread(url)
-            self.mw.threads.append(thread)
-            thread.file.connect(lambda data: _play(data))
-            thread.start()
 
         text_frame = QFrame()
         text_layout = QVBoxLayout()
@@ -189,35 +239,8 @@ class HorizontalMiniVoiceCard(CardFrame):
         play_button.setIcon(self.svg_icons.play())
         play_button.setFixedWidth(40)
         play_button.setStyleSheet("background-color: transparent; color: #e8eaed; border: none; font-size: 32px;")
-        play_button.clicked.connect(lambda event: play(self.data.get('previewAudioURI')))
+        play_button.clicked.connect(lambda _=False: _preview_controller(self.main_window).toggle(self.data.get('previewAudioURI'), play_button))
         card_layout.addWidget(play_button)
-
-        def set_play():
-            try:
-                play_button.setIcon(self.svg_icons.play())
-                play_button.clicked.connect(lambda event: play(self.data.get('previewAudioURI')))
-            except:
-                pass
-
-        def set_pause(thread):
-            try:
-                play_button.setIcon(self.svg_icons.pause())
-                play_button.clicked.connect(lambda event: thread.stop())
-            except:
-                pass
-
-        def _play(dataa):
-            thread = PlayerThread(dataa)
-            thread.play_signal.connect(lambda: set_pause(thread))
-            thread.stop_signal.connect(lambda: set_play())
-            self.main_window.threads.append(thread)
-            thread.start()
-
-        def play(url):
-            thread = FileLoaderThread(url)
-            self.main_window.threads.append(thread)
-            thread.file.connect(lambda dataa: _play(dataa))
-            thread.start()
 
         text_layout = QVBoxLayout()
         text_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
@@ -296,33 +319,8 @@ class VoiceSearch(QWidget):
         play_button = QPushButton()
         play_button.setIcon(self.svg_icons.play())
         play_button.setStyleSheet("background-color: transparent; border: none; padding: 8px;")
-        play_button.clicked.connect(lambda event: play(voice_data.get('previewAudioURI')))
+        play_button.clicked.connect(lambda _=False: _preview_controller(self.mw).toggle(voice_data.get('previewAudioURI'), play_button))
         card_layout.addWidget(play_button)
-
-        def set_play():
-            try:
-                play_button.setIcon(self.svg_icons.play())
-                play_button.clicked.connect(lambda event: play(voice_data.get('previewAudioURI')))
-            except: pass
-
-        def set_pause(thread):
-            try:
-                play_button.setIcon(self.svg_icons.pause())
-                play_button.clicked.connect(lambda event: thread.stop())
-            except: pass
-
-        def _play(dataa):
-            thread = PlayerThread(dataa)
-            thread.play_signal.connect(lambda: set_pause(thread))
-            thread.stop_signal.connect(lambda: set_play())
-            self.mw.threads.append(thread)
-            thread.start()
-
-        def play(url):
-            thread = FileLoaderThread(url)
-            self.mw.threads.append(thread)
-            thread.file.connect(lambda data: _play(data))
-            thread.start()
 
         text_layout = QVBoxLayout()
         card_layout.addLayout(text_layout, 1)
