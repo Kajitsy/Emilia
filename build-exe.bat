dc2846b8d61b927c1f991677b3708f0a93f6a9b6
Kajitsy <kajitsy@icloud.com>
2026-03-08 18:16:15 +0300

    Add OpenGL hidden-import and PyOpenGL deps
    Include OpenGL as a hidden-import in the PyInstaller build command to ensure OpenGL modules are bundled, and add pyopengl and pyopengl-accelerate to requirements.txt so the runtime dependencies are installed. Changes touch build-exe.bat (pyinstaller args) and requirements.txt (new packages).

--- build-exe.bat
+++ build-exe.bat
@@ -18,7 +18,7 @@ if errorlevel 1 (
 
 echo Starting compilation of main.py...
 @echo on
-pyinstaller --noconfirm --onedir --windowed --icon ".\src\icon.ico" --uac-admin --add-data ".venv/Lib/site-packages/livekit/rtc/resources/livekit_ffi.dll;livekit/rtc/resources" --hidden-import "urllib3.contrib.resolver.system" --hidden-import "urllib3.contrib.hface.protocols.http1" --hidden-import "urllib3.contrib.hface.protocols.http2" --hidden-import "livekit.rtc.resources" ".\src\main.py"
+pyinstaller --noconfirm --onedir --windowed --icon ".\src\icon.ico" --uac-admin --add-data ".venv/Lib/site-packages/livekit/rtc/resources/livekit_ffi.dll;livekit/rtc/resources" --add-data ".venv/Lib/site-packages/live2d/v3/FrameworkShaders;live2d/v3/FrameworkShaders" --hidden-import "urllib3.contrib.resolver.system" --hidden-import "urllib3.contrib.hface.protocols.http1" --hidden-import "urllib3.contrib.hface.protocols.http2" --hidden-import "OpenGL" ".\src\main.py"
 @echo off
 if exist ".\dist\main\main.exe" (
     echo Renaming main.exe to emilia.exe...
