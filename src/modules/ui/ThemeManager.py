e22b86d4fd7b2cb7a0a6eab6f947150711fc5ffa
Kajitsy <kajitsy@icloud.com>
2026-03-09 13:38:03 +0300

    Add recovery dark theme

--- src/modules/ui/ThemeManager.py
+++ src/modules/ui/ThemeManager.py
@@ -1,4 +1,4 @@
-import ctypes, json, os, platform
+import json, os
 
 from PyQt6.QtCore import QObject, pyqtSignal
 
@@ -9,6 +9,25 @@ class ThemeManager(QObject):
         super().__init__()
         self.current = "Dark"
         self.theme_path = "themes"
+        self.recovery_theme = {
+            "name": "Dark",
+            "titlebar": "dark",
+            "colors": {
+                "element_bg": "#494a4d",
+                "text": "#e8eaed",
+                "hover_bg": "#5f6368",
+                "pressed_bg": "#3c3d3f",
+                "disabled_text": "#a2a2ac",
+                "menu_bg": "#202024",
+                "menu_item_select": "#25262b",
+                "primary_bg": "#303134",
+                "scroll_sub": "#f0f0f0",
+                "scroll_handle": "#555",
+                "scroll_hover": "#777",
+                "mw_back": "#202124",
+                "mw_color": "#e8eaed"
+            }
+        }
 
     def c(self, key):
         return self.get_theme(self.current)['colors'][key]
@@ -50,9 +69,12 @@ class ThemeManager(QObject):
         return themes
 
     def get_theme(self, theme_name):
+        if theme_name not in self.get_themes():
+            return self.recovery_theme
         for root, _, files in os.walk(self.theme_path):
             for file in files:
                 if file.endswith(".json"):
                     theme = json.load(open(os.path.join(root, file)))
                     if theme.get('name') == theme_name:
                         return theme
+        return {}
