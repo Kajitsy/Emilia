0fe4a40230dd1d648bf80f9bfd559ddd036a3fe8
Kajitsy <kajitsy@icloud.com>
2026-03-10 22:12:54 +0300

    Update THemeManager

--- src/modules/ui/ThemeManager.py
+++ src/modules/ui/ThemeManager.py
@@ -1,14 +1,13 @@
-import json, os
+import json, os, sys
 
 from PyQt6.QtCore import QObject, pyqtSignal
+from pathlib import Path
 
 class ThemeManager(QObject):
     theme_changed = pyqtSignal()
 
     def __init__(self):
         super().__init__()
-        self.current = "Dark"
-        self.theme_path = "themes"
         self.recovery_theme = {
             "name": "Dark",
             "titlebar": "dark",
@@ -28,15 +27,57 @@ class ThemeManager(QObject):
                 "mw_color": "#e8eaed"
             }
         }
+        self.current = ""
+        self.theme_path = "themes"
+        self.colors_file = {}
+        self.styles_file = {}
+        self.default_styles = {}
 
     def c(self, key):
-        return self.get_theme(self.current)['colors'][key]
+        if key in self.colors_file:
+            return self.colors_file.get(key, "")
+        else:
+            return  self.recovery_theme["colors"].get(key, "")
+
+    def get_style(self, element):
+        if element in self.styles_file:
+            return self.styles_file.get(element, "")
+        else:
+            return self.default_styles.get(element, "")
 
     def set_theme(self, theme_name):
         themes = self.get_themes()
         if theme_name in themes and self.current != theme_name:
+            self.styles_file.clear()
+            self.colors_file.clear()
+
             self.current = theme_name
+            file = self.get_theme(self.current)
+            self.colors_file = file['colors']
+            use_def_qss = file.get('use_default_elements', True)
+            main_dir = Path(sys.argv[0]).resolve().parent
+            if use_def_qss:
+                self.load_default_qss()
+                self.styles_file = self.default_styles
+            else:
+                self.load_default_qss()
+                for element, path in file.get('elements', {}).items():
+                    style = open(main_dir / "themes" / self.current / path, "r", encoding="utf-8").read()
+                    for key, value in self.colors_file.items():
+                        style = style.replace(f"@{key}", value)
+                    self.styles_file[element] = style
             self.theme_changed.emit()
+
+    def load_default_qss(self):
+        self.default_styles = {}
+        main_dir = Path(sys.argv[0]).resolve().parent
+        default_qss_dir = main_dir / "data" / "default_qss"
+        for qss_file in default_qss_dir.rglob("*.qss"):
+            element = qss_file.stem
+            style = qss_file.read_text(encoding="utf-8")
+            for key, value in self.colors_file.items():
+                style = style.replace(f"@{key}", value)
+            self.default_styles[element] = style
 
     def get_themes_count(self):
         count = 0
