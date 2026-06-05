edb739010031af038756b2ec6659a70c8ab63b81
Kajitsy <kajitsy@icloud.com>
2026-06-05 19:56:48 +0300

    Update Version

--- src/modules/ui/mainwindow/MainPage.py
+++ src/modules/ui/mainwindow/MainPage.py
@@ -60,7 +60,7 @@ class MainPage(QMainWindow):
         self.drpc_show_username = self.settings.value("discord_rpc/show_username", False, type=bool)
         self.drpc_show_current_page = self.settings.value("discord_rpc/show_current_page", True, type=bool)
         self.svg_icons = Svg()
-        self.version = "3.3.0"
+        self.version = "3.3.1"
         self.beta = version.parse(self.version).is_prerelease
 
         geometry = self.settings.value("main_window/geometry")
